a48b89235813cc90d4ca87b7b6c8de2253208833
Rishabh Bohra <93476337+Mystic1869@users.noreply.github.com>
2022-07-15 11:45:45 +0530

    Add files via upload

---
+++ secret.h
@@ -0,0 +1,10 @@
+#ifndef SECRET_H
+#define SECRET_H
+
+#define Wifiname "Jarvis"
+#define Password "Rishabh1869"
+#define Username "MysticUwU"
+#define DISCORD_URL "https://discord.com/api/webhooks/996385679925846116/-Mvg1Y-ss_qL14VP9O2anQT37xC2g_MuEHwsnhm6ZIBTjBScIErOfe3lfRkxBtqM9D9z"
+#define DISCORD_TTS "false"
+#define miningKey "Rishabh1869"
+#endif
