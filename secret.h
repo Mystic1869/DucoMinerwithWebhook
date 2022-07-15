16150065afabaf96b29a0f9eabfba3021eba29c8
Rishabh Bohra <93476337+Mystic1869@users.noreply.github.com>
2022-07-15 12:02:29 +0530

    Update secret.h

--- secret.h
+++ secret.h
@@ -1,10 +1,10 @@
 #ifndef SECRET_H
 #define SECRET_H
 
-#define Wifiname "Jarvis"
-#define Password "Rishabh1869"
-#define Username "MysticUwU"
-#define DISCORD_URL "https://discord.com/api/webhooks/996385679925846116/-Mvg1Y-ss_qL14VP9O2anQT37xC2g_MuEHwsnhm6ZIBTjBScIErOfe3lfRkxBtqM9D9z"
+#define Wifiname "YourWifiName"
+#define Password "YourWifiPassword"
+#define Username "YourDucoUsername"
+#define DISCORD_URL "https://discord.com/api/webhooks/YourWebhookURL"
 #define DISCORD_TTS "false"
-#define miningKey "Rishabh1869"
+#define miningKey "" //mining key if you have any
 #endif
